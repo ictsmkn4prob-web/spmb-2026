71eff00962a0b06c9c868f3186dde442bac6a9b3
ictsmkn4prob-web <ict.smkn4prob@gmail.com>
2026-05-17 22:11:07 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyuqepoXxTfvxrDo5FBKzL8P7CyOQSoO-_zh3f1AlGIBWsqJlmvIhYxwmD6G0iHakBU/exec"; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwDtTCxVfHMa2BlbXKNLgrKzRwp0-R6wFN7e6xyMGzVWz7Djq-uZZ7kF7lrI2OoxMGHnA/exec"; 
 
 export interface FormField {
   id: string;
